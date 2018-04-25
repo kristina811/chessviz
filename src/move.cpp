ec18b0a80d2e984a12aabac566a417b1280512d1
kristina <kristinamascow1@gmail.com>
2018-04-25 11:42:03 +0700

    Some changes for move.cpp

--- src/move.cpp
+++ src/move.cpp
@@ -5,16 +5,16 @@
 void move(struct Board1 *A){
     int Y_first, Y_last;
     char X_first, X_last;
-    char temp1;
+    char temp;
     printf("\n enter your move: ");
     scanf("%c%d - %c%d", &X_first, &Y_first, &X_last, &Y_last);
     for(int i = 0; i < N; i++){
         if((A[i].X == X_last) && (A[i].Y == Y_last)) {
-            temp1 = A[i].name_f;
+            temp = A[i].name_f;
         for(int j = 0; j < N; j++){
             if((A[j].X == X_first) && (A[j].Y == Y_first)){
                 A[i].name_f = A[j].name_f;
-                A[j].name_f = temp1;
+                A[j].name_f = temp;
             }
         }
         }
